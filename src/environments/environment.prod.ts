0f78d04ef2dc3db2ff6348f11a9cdab0e0cea258
jenali <jenalivora22@gmail.com>
2025-05-06 14:36:11 +0530

    change api url

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+// src/environments/environment.prod.ts
+
+export const environment = {
+    production: true, // <-- Should be true for production builds
+    apiUrl: 'https://your-production-api-endpoint.com' // <-- CHANGE THIS to your actual production API URL
+    // Add other production-specific variables if needed
+  };
