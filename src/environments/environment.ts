e3207574a2e67ff73a7bb0dd0d8589bddbdb952d
jenali <jenalivora22@gmail.com>
2025-05-06 17:48:00 +0530

    change envirment file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 // src/environments/environment.ts (or environment.development.ts)
 
 export const environment = {
-    production: false, // Keep this as false for the development file
+    production: true, // Keep this as false for the development file
     apiUrl: 'https://telegram-bot-tool.onrender.com/' // <-- Add your development API URL here
     // Add other environment-specific variables if needed
   };
