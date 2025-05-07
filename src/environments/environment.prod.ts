a55f8cd21832f67e7336caf8d36dc4fd5f15fba2
jenali <jenalivora22@gmail.com>
2025-05-07 14:52:46 +0530

    Add url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 
 export const environment = {
     production: true, // <-- Should be true for production builds
-    apiUrl: 'https://your-production-api-endpoint.com' // <-- CHANGE THIS to your actual production API URL
+    apiUrl: 'https://telegram-bot-tool.onrender.com/' // <-- CHANGE THIS to your actual production API URL
   };
