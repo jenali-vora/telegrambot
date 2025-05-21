8fbafbc949a1eef67a08f21ed0758ed013c31c5f
jenali <jenalivora22@gmail.com>
2025-05-21 14:34:49 +0530

    hero section changes and font add color

--- src/app/component/contact-us/contact-us.component.ts
+++ src/app/component/contact-us/contact-us.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TestimonialSectionComponent } from "../../shared/component/testimonial-section/testimonial-section.component";
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [TestimonialSectionComponent],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.css'
 })
